bbb14ef2eb069b63bc687c35cb898675655e96a5
crazyxangel <37293964+crazyxangel@users.noreply.github.com>
2019-07-22 14:15:54 +0200

    update draw area

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import serial
 step_rev = 200          #steps for nema 17 to complete a revolution
-size = 100              #size of the plotter drawing area
+size = 300              #size of the plotter drawing area
 pulley_diameter = 15    #diameter of the pulley
 
 factor = size/100       #factor to translate the coordinates which are in % to the size of the drawing area
@@ -18,10 +18,9 @@ data = open("data.txt","r")
 string = data.read()
 string = string.replace(';', ',')
 output = open("changedforsplit.txt","w")
-output.write(string)
+output.write(string) 
 output.close()
 values = string.split(',')
-
 x = 0
 z = 0
 for y in values:
@@ -30,18 +29,18 @@ for y in values:
             z +=1
             if x < 5: 
                 x_coords.append(float(values[x]))
-                x_steps.append(round((float(values[x])/mmstep)))
+                x_steps.append(round(((float(values[x])/mmstep)*factor)))
             elif not float(values[x]) == float(values[x-3]) and not float(values[x-2]) == float(values[x-5]):
                 x_coords.append(float(values[x]))
-                x_steps.append(round((float(values[x])/mmstep)))
+                x_steps.append(round(((float(values[x])/mmstep)*factor)))
         elif z == 1:
             z +=1
             if x < 5: 
                 y_coords.append(float(values[x]))
-                y_steps.append(round((float(values[x])/mmstep)))
+                y_steps.append(round(((float(values[x])/mmstep)*factor)))
             elif not float(values[x]) == float(values[x-3]) and not float(values[x-1]) == float(values[x-4]):
                 y_coords.append(float(values[x]))
-                y_steps.append(round((float(values[x])/mmstep)))
+                y_steps.append(round(((float(values[x])/mmstep)*factor)))
         elif z == 2:
             z = 0
             thickness.append(float(values[x]))
