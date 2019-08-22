e63ae44827179675743a1c4bda5df854f5d84cc9
crazyxangel <37293964+crazyxangel@users.noreply.github.com>
2019-08-22 09:37:49 +0200

    Final version cleaned up
    22/08/2019

--- translater file to steps.py
+++ translater file to steps.py
@@ -2,7 +2,7 @@
 import serial
 from time import sleep
 
-ser = serial.Serial("COM17",115200,timeout = 0) #starting up the serial communication
+ser = serial.Serial("COM5",115200,timeout = 0) #starting up the serial communication
 
 step_rev = 200                                  #steps for nema 17 to complete a revolution
 size = 300                                      #size of the plotter drawing area
@@ -21,8 +21,8 @@ y_steps = []
 thickness = []                                  #thickness list currently gets data but has no use
 
 #dummy list for testing
-x_dummy = [10,20,10,20]
-y_dummy = [10,20,10,20]
+x_dummy = [500]
+y_dummy = [500]
 
 loop = True                                     #boolean for loop
 
@@ -47,18 +47,18 @@ for y in values:                                #loops trough entire values list
             z +=1
             if x < 5:                                                                                           #first coordinate cant be checked by previous one
                 x_coords.append(float(values[x]))                                                               #appends the coordinal value 
-                x_steps.append(round(((float(values[x])/mmstep)*factor)))                                       #appends the step value
+                x_steps.append(int(round(((float(values[x])/mmstep)*factor))))                                  #appends the step value
             elif not float(values[x]) == float(values[x-3]) and not float(values[x+1]) == float(values[x-2]):   #compares this row of x and y to the previous one to prevent duplicate coordinates
                 x_coords.append(float(values[x]))
-                x_steps.append(round(((float(values[x])/mmstep)*factor)))
+                x_steps.append(int(round(((float(values[x])/mmstep)*factor))))
         elif z == 1:                            # Y
             z +=1
             if x < 5: 
                 y_coords.append(float(values[x]))
-                y_steps.append(round(((float(values[x])/mmstep)*factor)))
+                y_steps.append(int(round(((float(values[x])/mmstep)*factor))))
             elif not float(values[x]) == float(values[x-3]) and not float(values[x-1]) == float(values[x-4]):
                 y_coords.append(float(values[x]))
-                y_steps.append(round(((float(values[x])/mmstep)*factor)))
+                y_steps.append(int(round(((float(values[x])/mmstep)*factor))))
         elif z == 2:                            #thickness
             z = 0
             thickness.append(float(values[x]))
@@ -90,8 +90,8 @@ while 'Ready' not in response:                  #waits until it gets a "ready" f
     response = str(ser.readline())
 
 print("ready")
-dataout = '/'+str(x_steps[0])+'x'+'/'+str(y_steps[0])+'y.'  #creates the text for the first serial write this happens again in the upcoming loop but this ensures the first coordinate is fully and correctly received
-# dataout = '/'+str(x_dummy[0])+'x'+'/'+str(y_dummy[0])+'y.'
+#dataout = '/'+str(x_steps[0])+'x'+'/'+str(y_steps[0])+'y.'  #creates the text for the first serial write this happens again in the upcoming loop but this ensures the first coordinate is fully and correctly received
+dataout = '/'+str(x_dummy[0])+'x'+'/'+str(y_dummy[0])+'y.'
 
 ser.write(dataout.encode())                     #sends out above mentioned text
 i = 0                                           #creates a variable for upcoming loop
@@ -100,16 +100,16 @@ prev = ''                                       #creates a blank string(diffiren
 while loop:
     # inputstring = input()                      #uncomment to manually enter serial data to send
     # ser.write(inputstring.encode())            #writes above mentioned data
-    # while i < len(y_dummy):                    # uncomment to send dummy data for easy testing
-    while i <= len(y_steps):                    # uncomment to send actual file data
+    while i < len(y_dummy):                    # uncomment to send dummy data for easy testing
+    #while i <= len(y_steps):                    # uncomment to send actual file data
         response = str(ser.readline())
         sleep(.010)                             #halts the program for a short moment to give serial data coming back some time
         if response != prev :                   #checks if serial buffer is not equal to the previous message
             print(response)                     #prints response to console for debugging use
             prev = response                     #sets the current message as previous reference
         if 'N' in response:                     #checks if microcontroller has send a N to comfirm its ready for next set of coordinates
-            dataout = '/'+str(x_steps[i])+'x'+'/'+str(y_steps[i])+'y.'  #forms the outgoing string
-            # dataout = '/'+str(x_dummy[i])+'x'+'/'+str(y_dummy[i])+'y.'
+            #dataout = '/'+str(x_steps[i])+'x'+'/'+str(y_steps[i])+'y.'  #forms the outgoing string
+            dataout = '/'+str(x_dummy[i])+'x'+'/'+str(y_dummy[i])+'y.'
             print(dataout)                      #prints the string to console for debugging use
             ser.write(dataout.encode())         #serially writes the string
             i += 1
